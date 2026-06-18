153083fa3416e83910d0a53b2626dd95279711be
Praful Rakhade <prafulrakhade02@gmail.com>
2026-06-18 16:03:32 +0530

    issue:903 local build fix testing (#915)
    Signed-off-by: Prafulrakhade <prafulrakhade02@gmail.com>

--- deploy/inji-certify/install.sh
+++ deploy/inji-certify/install.sh
@@ -13,7 +13,7 @@ echo "Create $SOFTHSM_NS namespace"
 kubectl create ns $SOFTHSM_NS
 
 NS=inji-certify
-CHART_VERSION=0.14.0
+CHART_VERSION=0.0.1-develop
 
 echo "Create $NS namespace"
 kubectl create ns $NS
